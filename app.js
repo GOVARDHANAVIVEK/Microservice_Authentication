cc91d1088be4d4c2bca5afa7864e8b3c0a8eed18
Vivek Govardhana <govardhana.vivek@mphasis.com>
2025-01-21 14:59:11 +0530

    latest

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const auth = require('./routes/auth')
-const dashboardRouter = require('./routes/dashboard')
+
 const dotenv = require('dotenv')
 dotenv.config()
 app.use(express.json())
